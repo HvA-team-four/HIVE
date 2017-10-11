349de330f2c5870cb02d7124cfb3ff1dab39481c
Unknown <jurderijp@live.nl>
2017-10-11 15:42:28 +0200

    Connecting to tor finished.

--- crawler/tor.py
+++ crawler/tor.py
@@ -1,17 +1,15 @@
-import socket
 import socks
+import socket
 
 
-def connectingtor():
-    # Setting up the TOR proxy
-    socksport = 9050  # The TOR proxy port that is default from torrc
+# Perform DNS resolution through the socket to translate the DNS names to IP addresses
+def getaddrinfo(*args):
+    return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
 
-    socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", socksport)  # Defines the version, the host, the port
-    socket.socket = socks.socksocket  # It builds a socket to TOR over the proxy client.
 
-    # Perform DNS resolution through the socket to translate the DNS names to IP addresses
-    def getaddrinfo(*args):  # create a tuple for
-        return [(socket.AF_INET, socket.SOCK_STREAM, 6, '',
-                 (args[0], args[1]))]  # Create the parameters for the socket.getaddrinfo
+# Setup the Tor proxy, the socket and perform the dns resolution to translates the domain name into a IPV4 address
+def connect_to_tor():
+    socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9050)
+    socket.socket = socks.socksocket
+    socket.getaddrinfo = getaddrinfo
 
-    socket.getaddrinfo = getaddrinfo  # See https://docs.python.org/2/library/socket.html for documentation
