7b7dc6a9cda37c0a2d59acbf0a91cc31a7dedfef
Unknown <lars.horchner@hva.nl>
2017-10-18 11:55:05 +0200

    Added errorlogging to contentcrawl
    Added error logging to contentcrawl

--- crawler/contentcrawl.py
+++ crawler/contentcrawl.py
@@ -3,7 +3,8 @@ import urllib.error
 import urllib
 import socks
 import socket
-from bs4 import BeautifulSoup
+import logging
+from socket import timeout
 
 url_list=[
   'http://74ypjqjwf6oejmax.onion/',
@@ -11,6 +12,11 @@ url_list=[
   'http://dppmfxaacucguzpc.onion/',
   'http://3g2upl4pq6kufc4m.onion/'
 ]
+
+#Setting up logging
+logging.basicConfig(filename='logging.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S')
+logging.info('\n----------------------------------------------------------------------------------------'
+             '\n Logging started')
 
 #TOR SETUP GLOBAL Vars
 SOCKS_PORT = 9050  # TOR proxy port that is default from torrc, change to whatever torrc is configured to
@@ -26,21 +32,24 @@ def getaddrinfo(*args):
 
 socket.getaddrinfo = getaddrinfo
 
-
-for i in range(len(url_list)): # Loop trough the array of Onion URL's
+i = 1
+for url in url_list: # Loop trough the array of Onion URL's
     webcontent = None
-    print("Crawling URL:"+url_list[i])#Print URL die wordt gescraped
-    req = urllib.request.Request(url_list[i])# request to onion site
+    print("Crawling URL:"+url)#Print URL die wordt gescraped
+    logging.info('Trying to open ' + url)#Plaats in logfile
 
+    # request to onion site, open url and read contents
     try:
-        response = urllib.request.urlopen(req, timeout=10)
-        webcontent = response.read()
-        print(webcontent)
+        webcontent = urllib.request.urlopen(url, timeout=10).read()
     except (urllib.error.HTTPError, urllib.error.URLError) as error:
-        print("Error retrieving website: ", url_list[i])
+        print("Error retrieving website: ", url)
+        logging.error('Data not retrieved from URL: ' + url + ' because error ' + str(error))
+    except timeout:
+        print("timeout")
 
+    if webcontent is not None:
+        logging.info('Contents of website written to: ' + (str(i) + '.txt'))
+        text_file = open(str(i) + ".txt", "w")
+        text_file.write("%s" % webcontent)
+        i += 1
 
-    """ TODO
-    TEMP save in textfile
-    betere errorhandling met log file
-    """
