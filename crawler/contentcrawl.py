6a2c8baee4952b2c82e6847b2cd0c8799a10adc9
Unknown <jurderijp@live.nl>
2017-10-18 13:01:50 +0200

    content_crawler function is finished

--- crawler/contentcrawl.py
+++ crawler/contentcrawl.py
@@ -1,42 +1,26 @@
 import urllib.request
 import urllib.error
 import urllib
-import socks
-import socket
 import logging
-from socket import timeout
-
-url_list=[
-  'http://74ypjqjwf6oejmax.onion/',
-  'http://idnxcnkne4qt76tg.onion/',
-  'http://dppmfxaacucguzpc.onion/',
-  'http://3g2upl4pq6kufc4m.onion/'
-]
-
-#Setting up logging
-logging.basicConfig(filename='logging.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S')
-logging.info('\n----------------------------------------------------------------------------------------'
-             '\n Logging started')
-
-#TOR SETUP GLOBAL Vars
-SOCKS_PORT = 9050  # TOR proxy port that is default from torrc, change to whatever torrc is configured to
-
-socks.set_default_proxy(socks.SOCKS5, "127.0.0.1",SOCKS_PORT)
-socket.socket = socks.socksocket
+import socket
 
 
-# Perform DNS resolution through the socket
-def getaddrinfo(*args):
-  return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+def setup_logfile(name):
+    # Setting up logging
+    logging.basicConfig(filename=str(name) + '.log', level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:%M:%S')
+    logging.info('\n----------------------------------------------------------------------------------------'
+                 '\n Logging started')
 
 
-socket.getaddrinfo = getaddrinfo
+def content_crawler(url):
+    # crawls the content
+    setup_logfile("content")
 
-i = 1
-for url in url_list: # Loop trough the array of Onion URL's
     webcontent = None
-    print("Crawling URL:"+url)#Print URL die wordt gescraped
-    logging.info('Trying to open ' + url)#Plaats in logfile
+
+    print("Crawling URL:" + url)
+    logging.info('Trying to open ' + url)
 
     # request to onion site, open url and read contents
     try:
@@ -44,12 +28,7 @@ for url in url_list: # Loop trough the array of Onion URL's
     except (urllib.error.HTTPError, urllib.error.URLError) as error:
         print("Error retrieving website: ", url)
         logging.error('Data not retrieved from URL: ' + url + ' because error ' + str(error))
-    except timeout:
+    except socket.timeout:
         print("timeout")
-
-    if webcontent is not None:
-        logging.info('Contents of website written to: ' + (str(i) + '.txt'))
-        text_file = open(str(i) + ".txt", "w")
-        text_file.write("%s" % webcontent)
-        i += 1
-
+        logging.error('Socket timed out, unable to retrieve data from URL: ' + url)
+    return webcontent
