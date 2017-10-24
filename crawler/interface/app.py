2f10ad3cf9935188a4f0aec4d47075190892cea0
Unknown <toinelambalk@outlook.com>
2017-10-24 16:43:18 +0200

    Style

--- crawler/interface/app.py
+++ crawler/interface/app.py
@@ -8,4 +8,5 @@ app.config.supress_callback_exceptions = True
 my_css_url = "https://raw.githubusercontent.com/HvA-team-four/HIVE/DashInterface/crawler/interface/assets/hive.css"
 app.css.append_css({
     "external_url": my_css_url
+
 })
