614ff3f57ba24cd6bb610949ed5f0c9f50837992
Unknown <toinelambalk@outlook.com>
2017-10-24 16:46:33 +0200

    Style

--- crawler/interface/index.py
+++ crawler/interface/index.py
@@ -12,6 +12,25 @@ from interface.elements import header
 
 app.layout = html.Div([
     header.hive_header,
+html.Div(
+    className='header',
+    children=html.Div(
+        className='container-width',
+        style={'height': '100%'},
+        children=[
+            html.Img(
+                src="https://lh3.googleusercontent.com/Ybh2Nnp3tz7A1hZgE96cNjAATd7kR7Fa9_A4ErsHMz8Pd7aYnUbstj7h4mqGXz398a9wkMtJ2XWOImk=w1686-h1302",
+                className="logo"
+            ),
+
+            html.Div(className="links", children=[
+                html.A('pricing', className="link", href="/pages/start"),
+                html.A('workshops', className="link", href="/pages/search")
+            ])
+        ]
+    )
+),
+
     dcc.Location(id='url', refresh = False),
     html.Div(id='page-content')
 ])
