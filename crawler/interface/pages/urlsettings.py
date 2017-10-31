84bf448ad40ac0ca8a1cdb7ca08e85356d887260
Unknown <toinelambalk@outlook.com>
2017-10-31 21:47:52 +0100

    Interface Changes and tests

--- crawler/interface/pages/urlsettings.py
+++ crawler/interface/pages/urlsettings.py
@@ -12,7 +12,7 @@ from crawler.models import *
 with db_session:
     results = select(p for p in Url)[:]
 
-    df = pd.DataFrame(columns=['URL','Date Added','Date Scan','Datum Scrape','Priority Scrape','Priority Scan'])
+    df = pd.DataFrame(columns=['URL','Date Added','Date Scan','Date Scrape','Priority Scrape','Priority Scan'])
 
     for result in results:
         if result.priority_scan == True:
@@ -27,19 +27,23 @@ with db_session:
         df = df.append({'URL': result.url,
                         'Date Added': result.date_added,
                         'Date Scan': result.date_scanned,
-                        'Datum Scrape': result.date_scraped,
+                        'Date Scrape': result.date_scraped,
                         'Priority Scrape': priorityscrape,
                         'Priority Scan': priorityscan}, ignore_index=True)
 
-
 layout = html.Div([
-    html.H3('Settings'),
+    html.H3('URL Settings'),
     html.P('On this page, you are able to add URLs to the database which will automatically receive a priority flag', style={'width':450}),
-    html.Div([dcc.Input(id='input-box', type="text", style={'width': 600}, placeholder='URL which need to be added to the database.'),
-    html.Button('Submit', id='button'),
+
+    html.Div([
+        dcc.Input(id='input-box', type="text", style={'width': 480}, placeholder='URL which need to be added to the database.'),
+        html.Button('Submit', id='button', style={'marginLeft': 20}),
+
+
     html.Br(),html.Br(),
-    html.Div(id='output-container-button',
-             children="")]),
+        html.Div(id='output-container-button', children="")]),
+
+
     html.Br(),
     html.Div(dt.DataTable(rows=df.to_dict('records'),
         sortable = True,
