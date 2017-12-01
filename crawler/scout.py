91ecf407825d82e94544a0157614d8d38fbe49b9
Unknown <toinelambalk@outlook.com>
2017-12-01 13:48:06 +0100

    Scout Content-Block

--- crawler/scout.py
+++ crawler/scout.py
@@ -5,40 +5,40 @@ from utilities.tor import connect_to_tor
 from utilities.url_util import format_url, get_urls_from_content
 from utilities.website import get_content_from_url
 
-
+# Add found URLs to the database if they are not being blocked by the content-block feature.
 @db_session
 def save_url(url):
-    result = select(p for p in Url if p.url == url).count()
-    if result == 0:
-        url_object = Url(
-            url=url,
-            date_added=datetime.now()
-        )
-        commit()
+    blockedUrls = select(b.value for b in Block if b.type == "Url")[:]
 
+    if not any(x for x in blockedUrls if x in url):
+        result = select(p for p in Url if p.url == url).count()
+        if result == 0:
+            url_object = Url(
+                url=url,
+                date_added=datetime.now()
+            )
+    else:
+        log.info("URL: {} is blocked".format(url))
 
+    commit()
+
+# Update the URL which was being scraped
 @db_session
 def update_url(url):
     url.date_scanned = datetime.now()
-    url.priority_scan = False
-
-
-@db_session
-def get_urls():
-    return select(u for u in Url if u.date_scanned is None).order_by(desc(Url.priority_scan))[:32]
-
 
 @db_session
 def start_scout():
-    log.debug('scout has been started')
+    log.debug('Scout has been started')
+
     while True:
-        urls = get_urls()
-        # Connect to tor after getting the urls. Otherwise mysql will be unreachable.
-        connect_to_tor()
+        urls = select(u for u in Url if u.date_scanned is None).order_by(desc(Url.priority_scan))[:32]
+
         if len(urls) == 0:
             print("No URLs to be crawled, waiting for 60 seconds.")
             log.info('No URLs to be crawled, waiting for 60 seconds.')
-            sleep(60)
+            sleep(10)
+            commit()
             continue
 
         for url in urls:
@@ -48,13 +48,17 @@ def start_scout():
 
                 for content_url in content_urls:
                     formatted_url = format_url(url.url, content_url)
+
                     if formatted_url is not None:
                         save_url(formatted_url)
 
             except(ValueError, NameError, TypeError) as error:
                 log.error(str(error))
+
             finally:
                 update_url(url)
+
+
 
 
 if __name__ == '__main__':
