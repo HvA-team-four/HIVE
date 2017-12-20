ebe5f58b4f8d823295283ba5be2c869b3fb843ff
Unknown <toinelambalk@outlook.com>
2017-12-20 13:00:17 +0100

    User Guidw

--- crawler/interface/pages/userguide.py
+++ crawler/interface/pages/userguide.py
@@ -6,7 +6,7 @@ import dash_core_components as dcc
 
 
 def user_guide(value):
-    if value == 1: # Welcome page
+    if value == 1:  # Welcome page
         return html.Div([
             dcc.Markdown('''
 #### Start
@@ -58,19 +58,19 @@ On the  [**URL Settings** page](/pages/urlsettings), you are able to view the fo
 
 ###### Statistics
 The statistics provide you with insight in the amount of URLs in the database and the percentage of scaned and
-scraped URLs. The button **REFRESH STATISTICS** can be used to refresh the statistics. The application does
+scraped URLs. The button `Refresh Statistics` can be used to refresh the statistics. The application does
 not automatically perform tasks such as refreshing or loading information to make the application as fast 
 as possible. The statistics are also automatically loaded when the application is started. 
 
 ###### Add URLs
 You can add URLs to the database by entering a value in the field which says: *URL* which need to be added to
-the database. Use the **SUBMIT** button to commit the value and add it to the database. The application
+the database. Use the `Submit` button to commit the value and add it to the database. The application
 automatically checks whether the URL already exists or not. Added URLs will automatically receive the
 Priority Scrape and Priority Scan flag. 
 
 ###### Load table
 The table is not loaded by default, this done because this can take a while depending on the amount of URLs 
-in the database. When using the **LOAD TABLE** button, the application will try to retrieve *all* URLs in
+in the database. When using the `Load Table` button, the application will try to retrieve all URLs in
 the database. When the application is loading the table, you will not be able to perform other tasks. '''),
         ])
 
@@ -82,27 +82,27 @@ On the  [**Keyword Settings** page](/pages/keywordsettings), you are able to vie
 
 ###### Statistics
 The statistics provide you with insight in the amount of Keywords in the database and the percentage of active keywords.
-The button **REFRESH STATISTICS** can be used to refresh the statistics. The application does
+The button `Refresh Statistics` can be used to refresh the statistics. The application does
 not automatically perform tasks such as refreshing or loading information to make the application as fast 
 as possible. The statistics are also automatically loaded when the application is started. 
 
 ###### Add Keywords
 You can add Keywords to the database by entering a value in the field which says: *Keyword* which need to be added to
-the database. Use the **SUBMIT** button to commit the value and add it to the database. The application
+the database. Use the `Submit` button to commit the value and add it to the database. The application
 automatically checks whether the Keyword already exists or not. Added Keywords will be set active by default. 
 
 ###### Load table
 The table is not loaded by default, this done because this can take a while depending on the amount of Keywords 
-in the database. When using the **LOAD TABLE** button, the application will try to retrieve *all* Keywords in
+in the database. When using the `Load Table` button, the application will try to retrieve *all* Keywords in
 the database. When the application is loading the table, you will not be able to perform other tasks. 
 
 ###### Filter table
-By using the **FILTER ROWS** button, you are able to show fields which can be used to filter information in the table. 
+By using the `Filter Rows` button, you are able to show fields which can be used to filter information in the table. 
 
 ###### Active/Inactive
-The active and inactive buttons can be used to set keywords active or inactive. Inactive keywords will not be matched
-any longer but won't be removed from the database as well. As a user, you will still be able to search for content that 
-was matched before you set the keyword inactive.'''),
+The `Set Active` and `Set Inactive` buttons can be used to set keywords active or inactive. Inactive keywords will not 
+be matched any longer but won't be removed from the database as well. As a user, you will still be able to search for 
+content that was matched before you set the keyword inactive.'''),
         ])
 
     elif value == 6:  # Content-block settings
@@ -114,13 +114,13 @@ On the [**Keyword Settings** page](/pages/blocksettings), you are able to view t
 
 ###### Statistics
 The statistics provide you with insight in the amount of Block-rules in the database and the percentage of active rules.
-The button **REFRESH STATISTICS** can be used to refresh the statistics. The application does
+The button `Refresh Statistics` can be used to refresh the statistics. The application does
 not automatically perform tasks such as refreshing or loading information to make the application as fast 
 as possible. The statistics are also automatically loaded when the application is started. 
 
 ###### Add Rules
 You can add Rules to the database by entering a value in the input-field the database. Select the type of rule and use 
-the *SUBMIT* button to commit the value and add it to the database. The applicationautomatically checks whether the 
+the `Submit` button to commit the value and add it to the database. The application automatically checks whether the 
 Rule with the specified type already exists or not. Added Rules will be set active 
 by default. 
 
@@ -135,18 +135,40 @@ a URL matches or contains the specified URL, the URL is being ignored
 
 ###### Load table
 The table is not loaded by default, this done because this can take a while depending on the amount of Rules 
-in the database. When using the **LOAD TABLE** button, the application will try to retrieve *all* Rules in
+in the database. When using the `Load Table` button, the application will try to retrieve all Rules in
 the database. When the application is loading the table, you will not be able to perform other tasks. 
 
 ###### Filter table
-By using the **FILTER ROWS** button, you are able to show fields which can be used to filter information in the table. 
+By using the `Filter Rows` button, you are able to show fields which can be used to filter information in the table. 
 
 ###### Active/Inactive
-The active and inactive buttons can be used to set Rules active or inactive. Inactive keywords will not be matched
-any longer but won't be removed from the database as well. As a user, you will still be able to search for content that 
-Rules'''),
+The `Set Active` and `Set Inactive` buttons can be used to set Rules active or inactive. Inactive keywords will not be 
+matched any longer but won't be removed from the database as well. As a user, you will still be able to search for 
+content that match those Rules'''),
+        ])
+
+    elif value == 7:  # Configuration settings
+        return html.Div([
+            dcc.Markdown(''' 
+#### Configuration Settings
+On the [**Configuration Settings** page](/pages/configurationsettings), you are able to view the following sections:
 
 
+###### File location
+The configurations settings page displays the location of the active configuration file on the computer. Please note
+that without a configuration file, the application won't start successfully. Make sure to not remove the 
+configuration file from the defined location. It is not possible to change the location of the configuration file in
+this version of HIVE.
+
+###### Styling
+This section displays the location of the css file used for HIVE. This parameter can be changed in the configuration
+file. The CSS file needs to be hosted on an external location (a web-server), it is not possible to define a local
+CSS file. 
+
+###### Images
+This section displays the base path of the image location, the images need to be hosted on an external location
+(a web-server). The image folder is distributed in the application folder, make sure to not change the structure of
+the image directory.'''),
         ])
 
     else:
@@ -184,7 +206,8 @@ layout = html.Div([
 
             ],
             id='tabs',
-            vertical=True
+            vertical=True,
+            value=1
         )
     ], className="userguide_menu"),
 
